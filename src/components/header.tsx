a0b9953826bbf84fbf1cd69efb5f724b3b640ffe
lemonpole <srivera.x1@gmail.com>
2022-07-02 10:44:27 -0400

    feat: basic header implementation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { Box, Divider, Flex, IconButton, Text, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SearchIcon, SunIcon } from '@chakra-ui/icons';
+import { Box, IconButton, Stack, StackDivider, Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Constants } from '@dxtr/lib';
 
 
@@ -13,28 +13,44 @@ export default function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Flex
-      align="center"
+    <Box
+      as="header"
+      zIndex="docked"
+      pos="fixed"
+      top="0" left="0"
+      display="flex"
+      alignItems="center"
       justifyContent="space-between"
+      bg={useColorModeValue( Constants.Theme.COLOR_FOREGROUND[ 0 ], Constants.Theme.COLOR_FOREGROUND[ 1 ] )}
+      borderBottom="1px"
+      borderColor="inherit"
+      borderStyle="solid"
       h={Constants.Theme.HEADER_HEIGHT}
-      p={5}
+      w="full"
+      p={Constants.Theme.HORIZONTAL_RYTHM}
     >
-      <Box>
-        <Text>
-          <b>dex</b>terous
-        </Text>
-      </Box>
-      <Flex align="center" h="100%" gap="5">
-        <Divider orientation="vertical"/>
+      <Text>
+        <b>dex</b>terous
+      </Text>
+      <Stack
+        as="article"
+        direction="row"
+        divider={<StackDivider />}
+      >
         <IconButton
           aria-label={`Search ${Constants.Application.POKEMON_LABEL}`}
+          variant="ghost"
+          icon={<SearchIcon />}
+        />
+        <IconButton
+          aria-label="Toggle Dark/Light Theme"
           variant="ghost"
           icon={colorMode === Constants.Theme.MODE_LIGHT
             ? <MoonIcon onClick={toggleColorMode} />
             : <SunIcon onClick={toggleColorMode} />
           }
         />
-      </Flex>
-    </Flex>
+      </Stack>
+    </Box>
   );
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,4 +6,7 @@ export const Application = {
 export const Theme = {
   HEADER_HEIGHT: '60px',
   MODE_LIGHT: 'light',
+  COLOR_FOREGROUND: [ 'gray.50', 'gray.700' ],
+  VERTICAL_RYTHM: 2,
+  HORIZONTAL_RYTHM: 2,
 };
