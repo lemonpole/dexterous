a74d6695b2b5838c3cba7d7fc11cd07951e819b0
lemonpole <srivera.x1@gmail.com>
2022-07-25 08:55:32 -0400

    chore: update home page

---
+++ src/components/text-logo.tsx
@@ -0,0 +1,15 @@
+import { chakra } from '@chakra-ui/react';
+
+
+export default function TextLogo() {
+  return (
+    <chakra.span
+      fontWeight="hairline"
+      letterSpacing="wide"
+      cursor="default"
+    >
+      <chakra.b color="brand.300">dex</chakra.b>
+      terous
+    </chakra.span>
+  );
+}
