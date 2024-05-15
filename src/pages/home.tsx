2b3ad2a4b0eab9698c6a9c89201d971488524350
Steve Rivera <srivera.x1@gmail.com>
2024-05-15 10:32:29 -0400

    feat: home page is now two columns on large screens

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import PackageInfo from '@dxtr/package';
 import { Outlet } from 'react-router-dom';
 import { Constants } from '@dxtr/lib';
@@ -17,60 +18,73 @@ import {
 } from '@chakra-ui/react';
 
 /**
- * @component
- * @name Home
+ * The welcome component contains the about and support blurbs.
+ *
+ * @function
  */
+function Welcome() {
+  return (
+    <React.Fragment>
+      <Stack>
+        <Heading as="h2">About</Heading>
+        <Text>
+          An <em>ad-free</em> and lightweight Pokédex.
+        </Text>
+        <Text>
+          Contains the essentials (types, weaknesses, evolutions) and an
+          intuitive interface to seamlessly navigate through Pokédex entries
+          while playing your favorite Pokémon game.
+        </Text>
+      </Stack>
+      <Stack>
+        <Heading as="h2">Support / Contributing</Heading>
+        <List>
+          <ListItem>
+            <ListIcon as={InfoIcon} />
+            How does it work?&nbsp;
+            <ExternalLink href={PackageInfo.repository.url}>
+              View the source code.
+            </ExternalLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} />
+            Want to contribute?&nbsp;
+            <ExternalLink href={PackageInfo.repository.url + 'pulls'}>
+              Submit a Pull Request.
+            </ExternalLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={WarningIcon} />
+            Found a bug?&nbsp;
+            <ExternalLink href={PackageInfo.bugs.url}>
+              Please file an issue.
+            </ExternalLink>
+          </ListItem>
+        </List>
+      </Stack>
+    </React.Fragment>
+  );
+}
 
+/**
+ * The home component.
+ *
+ * @function
+ * @exports
+ */
 export default function Home() {
   return (
     <Box mt={Constants.Application.HEADER_HEIGHT} px="2" py="4">
       <Outlet />
 
-      <VStack align="flex-start">
-        <Heading as="h2">Featured</Heading>
-        <PokemonGrid />
-      </VStack>
-
-      <SimpleGrid spacing="5" textAlign="justify" columns={[1, null, 2]} pt="4">
-        <Stack>
-          <Heading as="h2">About</Heading>
-          <Text>
-            An <em>ad-free</em> and lightweight Pokédex.
-          </Text>
-          <Text>
-            Contains the essentials (types, weaknesses, evolutions) and an
-            intuitive interface to seamlessly navigate through Pokédex entries
-            while playing your favorite Pokémon game.
-          </Text>
-        </Stack>
-        <Stack>
-          <Heading as="h2" textAlign="left">
-            Support / Contributing
-          </Heading>
-          <List>
-            <ListItem>
-              <ListIcon as={InfoIcon} />
-              How does it work?&nbsp;
-              <ExternalLink href={PackageInfo.repository.url}>
-                View the source code.
-              </ExternalLink>
-            </ListItem>
-            <ListItem>
-              <ListIcon as={CheckCircleIcon} />
-              Want to contribute?&nbsp;
-              <ExternalLink href={PackageInfo.repository.url + 'pulls'}>
-                Submit a Pull Request.
-              </ExternalLink>
-            </ListItem>
-            <ListItem>
-              <ListIcon as={WarningIcon} />
-              Found a bug?&nbsp;
-              <ExternalLink href={PackageInfo.bugs.url}>
-                Please file an issue.
-              </ExternalLink>
-            </ListItem>
-          </List>
-        </Stack>
+      <SimpleGrid spacing="5" columns={{ sm: 1, xl: 2 }}>
+        <VStack align="flex-start">
+          <Heading as="h2">Featured</Heading>
+          <PokemonGrid />
+        </VStack>
+        <VStack align="flex-start" gap="5">
+          <Welcome />
+        </VStack>
       </SimpleGrid>
     </Box>
   );
